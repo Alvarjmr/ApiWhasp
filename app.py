be432b2c6876159d65a0149ed704f1ae32e502fa
Alvaro Martinez <ajmartinez8789@gmail.com>
2026-01-22 19:15:01 -0500

    webhookport1

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def recibir_mensaje(req):
     return jsonify({'status': 'EVENT_RECEIVED'}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
